7996090341ea4e2f697a6268bc893170bde34870
oscar galindo <oscar.galindo1@yahoo.com>
2026-02-19 13:00:00 -0600

    feat(auth): add guest middleware

---
+++ app/middleware/guest.ts
@@ -0,0 +1,9 @@
+export default defineNuxtRouteMiddleware(async (to) => {
+  const authStore = useAuthStore();
+
+  await authStore.initAuth();
+
+  if (authStore.isAuthenticated) {
+    return navigateTo("/");
+  }
+});
