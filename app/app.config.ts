38265bdf79b4e725c8e8370f6a17613938171223
oscar galindo <oscar.galindo1@yahoo.com>
2026-02-20 09:00:00 -0600

    chore(config): update app config

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,8 +1,9 @@
 export default defineAppConfig({
   ui: {
     colors: {
-      primary: 'sky',
-      neutral: 'zinc'
-    }
-  }
-})
+      neutral: 'neutral',
+      primary: 'emerald',
+      secondary: 'sky',
+    },
+  },
+});
