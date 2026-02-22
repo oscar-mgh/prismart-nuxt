cad024a201afde91bfc8c5260c7d30c155aa2454
oscar galindo <oscar.galindo1@yahoo.com>
2026-02-22 10:00:00 -0600

    chore(config): update nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,57 @@
 export default defineNuxtConfig({
-  modules: ["@nuxt/eslint", "@nuxt/ui", "@pinia/nuxt"],
+  modules: [
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/ui",
+    "@pinia/nuxt",
+    "@nuxtjs/google-fonts",
+  ],
+  googleFonts: {
+    families: {
+      Quicksand: [300, 400, 500, 600, 700],
+    },
+    display: "swap",
+    prefetch: true,
+    preconnect: true,
+  },
   devtools: {
     enabled: true,
   },
   css: ["~/assets/css/main.css"],
-  compatibilityDate: "2025-01-15",
   runtimeConfig: {
     public: {
       apiBase:
         process.env.NUXT_PUBLIC_API_BASE || "http://localhost:3000/api/v1",
+      websocketBase: process.env.NUXT_PUBLIC_WS_BASE || "http://localhost:3000",
+      stripePublishableKey:
+        process.env.NUXT_PUBLIC_STRIPE_PUBLISHABLE_KEY || "",
+      cartShippingFee: process.env.CART_SHIPPING_FEE || "90",
+      cartFreeShippingThreshold:
+        process.env.CART_FREE_SHIPPING_THRESHOLD || "400",
     },
   },
-  eslint: {
-    config: {
-      stylistic: {
-        commaDangle: "never",
-        braceStyle: "1tbs",
-      },
-    },
+  alias: {
+    shared: "./shared",
   },
+  devServer: {
+    port: 4000,
+  },
+  future: {
+    compatibilityVersion: 4,
+  },
+  compatibilityDate: "2025-01-15",
+  nitro: {
+    ignore: [
+      "/socket.io/**"
+    ]
+  }
+  // nitro: {
+  //   devProxy: {
+  //     "/socket.io": {
+  //       target: "http://localhost:3000",
+  //       ws: true,
+  //     },
+  //   },
+  // },
 });
